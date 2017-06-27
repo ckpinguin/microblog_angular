bb0ebf09b355c5d8c8606ef8c0fda7aceaa40b69
Christof Kälin <christof.kaelin@gmail.com>
2017-06-27 17:38:25 +0200

    updated script

--- count_operators.sh
+++ count_operators.sh
@@ -9,16 +9,28 @@ FUNCS='function |=> {|[a-z]+\\(.*\\)|'
 SPECIALS='import'
 
 PATTERN="$SCSPEC|$TYPE_QUAL|$RESERVED|$OPERATOR"
-EXCEPTIONS="$COMMENTS|$SPECIALS"
+EXCEPTIONS="$COMMENTS|$FUNCS|$SPECIALS"
 
 echo "File $1"
-echo "without filtering out:" 
+echo "no filtering:" 
 egrep -o "$PATTERN" $1 | sort -n | uniq -c
 echo
 echo "with filtering:"
-egrep -v "$SPECIALS" $1 | egrep -o "$PATTERN" | sort -n | uniq -c 
+#egrep -v "$EXCEPTIONS" $1 | egrep -o "$PATTERN" | sort -n | uniq -c 
+egrep -v "$EXCEPTIONS" $1
 echo
-echo "comments filter alone:"
-egrep -o "$COMMENTS" $1
+echo "exceptions filter:"
+egrep -o "$EXCEPTIONS" $1 | sort -n | uniq -c
 echo
+echo "grep COMMENTS:"
+egrep "$COMMENTS" $1
+echo
+echo "grep FUNCS:"
+egrep "$FUNCS" $1
+echo
+echo "grep SPECIALS:"
+egrep "$SPECIALS" $1
+echo
+echo "grep EXCEPTIONS:"
+egrep "$EXCEPTIONS" $1
 echo
