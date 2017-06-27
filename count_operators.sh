add111eb47c25787672c7bb0fbdbd3cf85f86d67
Christof Kälin <christof.kaelin@gmail.com>
2017-06-27 23:36:04 +0200

    working OK now for N1 in typescript files

--- count_operators.sh
+++ count_operators.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
 
 SCSPEC='static'
-TYPE_QUAL='const|let'
-RESERVED='for |if |import'
-OPERATOR='\!|\!=|%|%=|&|&&|&=|\|\||\(|\[|\*|;|{|\[|\*|\*=|\+|\+\+|\+=|,|-|--|-=|=>|\.|/|/=|:|<|<=|=|==|>|>=|\?|\||=&'
+TYPE_QUAL='const|let|private|protected|public'
+RESERVED='if |else|import |break|case|continue|default|do|for |new\(|return|sizeof|switch |while |this|try|catch|throw|throws|finally|instanceof|interface|extends|implements|abstract|true|false'
+OPERATOR="\!|\!=|%|%=|&|&&|&=|\|\||\(|\[|\*|;|{|\[|\*|\*=|\+|\+\+|\+=|,| -|--|-=|=>|[^\s']\.|[^'].+/|/=|:|<|<=|=|==|>|>=|\?|\||=&"
 COMMENTS='^ *//|^ */\*\*|^ *\*|^ *<\!--'
-FUNCS='function |=> {|[a-z]+\\(.*\\)|'
+FUNCS='function |[a-z]+\(.+\) \{'
 SPECIALS='import'
 
 PATTERN="$SCSPEC|$TYPE_QUAL|$RESERVED|$OPERATOR"
-EXCEPTIONS="$COMMENTS|$FUNCS|$SPECIALS"
+EXCEPTIONS="$COMMENTS|$FUNCS"
 
 echo "File $1"
-echo "no filtering:" 
+#cho "using ack:"
+#echo "=========="
+#ack "$PATTERN" $1
+#ack -c "$PATTERN" $1
+echo "no filtering:"
+echo "============="
 egrep -o "$PATTERN" $1 | sort -n | uniq -c
+echo -n "Total operators (unfiltered): "
+egrep -o "$PATTERN" $1 | sort -n | uniq -c | awk '{print $1}' | paste -sd+ | bc
 echo
 echo "with filtering:"
-#egrep -v "$EXCEPTIONS" $1 | egrep -o "$PATTERN" | sort -n | uniq -c 
-egrep -v "$EXCEPTIONS" $1
+echo "==============="
+egrep -v "$EXCEPTIONS" $1 | egrep -o "$PATTERN" | sort -n | uniq -c
+echo -n "Total operators N1: "
+egrep -v "$EXCEPTIONS" $1 | egrep -o "$PATTERN" | sort -n | uniq -c | awk '{print $1}' | paste -sd+ | bc
 echo
 echo "exceptions filter:"
+echo "=================="
 egrep -o "$EXCEPTIONS" $1 | sort -n | uniq -c
 echo
 echo "grep COMMENTS:"
+echo "=============="
 egrep "$COMMENTS" $1
 echo
 echo "grep FUNCS:"
+echo "==========="
 egrep "$FUNCS" $1
 echo
 echo "grep SPECIALS:"
+echo "=============="
 egrep "$SPECIALS" $1
 echo
 echo "grep EXCEPTIONS:"
+echo "================"
 egrep "$EXCEPTIONS" $1
 echo
