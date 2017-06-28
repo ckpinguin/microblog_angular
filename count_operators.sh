7d3486c8fc6283e6a0cdc7250437802b80776d28
Christof Kälin <christof.kaelin@gmail.com>
2017-06-28 17:09:06 +0200

    updated script and results

--- count_operators.sh
+++ count_operators.sh
@@ -2,34 +2,46 @@
 
 # Optimized for angular/typescript!
 SCSPEC='static'
+SCSPEC_ACK='(static)'
 TYPE_QUAL='const|let|private|protected|public'
-RESERVED='if |else|import |break|case|class |continue|default|do|for |new\(|return|sizeof|switch |while |this|try|catch|throw|throws|finally|instanceof|interface |extends |implements |abstract |true|false'
-OPERATOR="\!|\!=|%|%=|&|&&|&=|\|\||\(|\[|\*|;|{|\[|\*|\*=|\+|\+\+|\+=|,| -|--|-=|=>|[^']\.+|[^'].+/|/=|:|<|<=|=|==|>|>=|\?|\||=&"
+TYPE_QUAL_ACK='(const)|(let)|(private)|(protected)|(public)'
+RESERVED='@Component\(| if |else|import |break|case|class |continue|default|do|for |new\(|return|sizeof|switch |while |this|try|catch|throw|throws|finally|instanceof|interface |extends |implements |abstract |true|false'
+RESERVED_ACK='(@Component\()| (if) |(else)|(import) |(break)|(case)|(class) |(continue)|(default)|(do)|(for) |(new\()|(return)|(sizeof)|(switch) |(while) |(this)|(try)|(catch)|(throw)|(throws)|(finally)|(instanceof)|(interface) |(extends) |(implements) |(abstract) |(true)|(false)'
+#OPERATOR="(,)"
+OPERATOR="\!|\!=|%|%=|&|&&|&=|\|\||\(|\[|\*|;|\{|\[|\*|\*=|\+|\+\+|\+=|,| -|--|-=|=>|[^']\.+|[^'].+/|/=|:|<|<=|=|==|>|>=|\?|\||=&"
+OPERATOR_ACK="(,)|(\!)|(\!=)|(%)|(%=)|(&)|(&&)|(&=)|(\|\|)|(\()|(\[)|(\*)|(;)|(\{)|(\[)|(\*)|(\*=)|(\+)|(\+\+)|(\+=)| (-)|(--)|(-=)|(=>)|(?<!').*(\.+)|(?<!').+(\/)|(\/=)|(:)|(<)|(<=)|(=)|(==)|(>)|(>=)|(\?)|(\|)|(=&)"
 COMMENTS='^ *//|^ */\*\*|^ *\*|^ *<\!--'
 FUNCS='function |[a-z]+\(.+\) \{'
-SPECIALS='import'
+#SPECIALS='import'
 
 PATTERN="$SCSPEC|$TYPE_QUAL|$RESERVED|$OPERATOR"
+PATTERN_ACK="$SCSPEC_ACK|$TYPE_QUAL_ACK|$RESERVED_ACK|$OPERATOR_ACK"
 EXCEPTIONS="$COMMENTS|$FUNCS"
+
 
 echo "File: $1"
 echo "-----------------------------"
-#cho "using ack:"
-#echo "=========="
+
 #ack "$PATTERN" $1
 #ack -c "$PATTERN" $1
-echo "no filtering:"
-echo "============="
-egrep -o "$PATTERN" $1 | sort -n | uniq -c
-echo -n "Total operators (unfiltered): "
-egrep -o "$PATTERN" $1 | sort -n | uniq -c | awk '{print $1}' | paste -sd+ | bc
-echo
 echo "with filtering:"
 echo "==============="
 egrep -v "$EXCEPTIONS" $1 | egrep -o "$PATTERN" | sort -n | uniq -c
 echo -n "Total operators N1: "
 egrep -v "$EXCEPTIONS" $1 | egrep -o "$PATTERN" | sort -n | uniq -c | awk '{print $1}' | paste -sd+ | bc
 echo
+echo "no filtering:"
+echo "============="
+egrep -o "$PATTERN" $1 | sort -n | uniq -c
+echo -n "Total operators (unfiltered): "
+egrep -o "$PATTERN" $1 | sort -n | uniq -c | awk '{print $1}' | paste -sd+ | bc
+echo
+if [ "$2" == "v" ]; then
+    echo "using ack:"
+    echo "=========="
+    egrep -v "$EXCEPTIONS" $1 | ack "$PATTERN_ACK"
+    echo
+fi
 echo "exceptions filter:"
 echo "=================="
 egrep -o "$EXCEPTIONS" $1 | sort -n | uniq -c
@@ -42,10 +54,10 @@ echo "grep FUNCS:"
 echo "==========="
 egrep "$FUNCS" $1
 echo
-echo "grep SPECIALS:"
-echo "=============="
-egrep "$SPECIALS" $1
-echo
+# echo "grep SPECIALS:"
+# echo "=============="
+# egrep "$SPECIALS" $1
+# echo
 echo "grep EXCEPTIONS:"
 echo "================"
 egrep "$EXCEPTIONS" $1
